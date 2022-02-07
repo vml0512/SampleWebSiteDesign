219f8d778e736a8f9063567c7dafbfc289b4c464
vml0512 <vmlrajmh598@gmail.com>
2022-02-07 12:46:57 +0530

    Commit Sample project -Estee_Loyalty

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById("clickbutton").addEventListener("click",function(){
+document.querySelector(".popup").style.display="flex";
+})
+document.querySelector(".close").addEventListener("click",function(){
+document.querySelector(".popup").style.display="none";
+})
